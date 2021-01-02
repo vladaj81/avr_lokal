ef2d0a671a94affbb7546c1d4175fb2bbd0b14b7
vladaj81 <vladaj13@gmail.com>
2021-01-02 21:52:21 +0100

    Update konekcija_amso.php

--- dodaci/konekcija_amso.php
+++ dodaci/konekcija_amso.php
@@ -1,5 +1,5 @@
 <?php
-$amso_konekcija = pg_connect("host=10.101.50.12 dbname=amso user=zoranp");
+$amso_konekcija = pg_connect("host=10.101.50.12 dbname=a user=z");
 
 if (!$amso_konekcija) {
     exit('Greška otvaranja konekcije prema SQL serveru.');
